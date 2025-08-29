176b5fc258bd59881cd88c8d16445ab113eb1e52
PhG22 <d3athr4it0@gmail.com>
2025-08-28 21:09:42 -0300

    Started working on AV1

---
+++ Trabalho 1 - Arquitetura Orientada a Eventos e Criptografia Assimétrica/cliente.py
@@ -0,0 +1,26 @@
+import pika
+import sys
+
+def callback(ch, method, properties, body):
+    print(f" [x] {method.routing_key}:{body}")
+
+
+class Client:
+    def __init__(self):
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+        result = channel.queue_declare(queue='leilao_iniciado', exclusive=True)
+        queue_name = result.method.queue
+        channel.queue_bind(
+        exchange='direct_logs', queue=queue_name, routing_key="")
+
+        channel.basic_consume(
+            queue=queue_name, on_message_callback=callback, auto_ack=True)
+        
+
+    def make_bid(self):
+        pass
